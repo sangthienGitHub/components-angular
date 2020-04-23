ea5d85941f25120e18b68124cac665194e6cf3f7
sangTran <sangthien2010@gmail.com>
2020-04-22 19:59:34 -0500

    show statistics component on top of Views Module

--- src/app/views/views-home/views-home.component.ts
+++ src/app/views/views-home/views-home.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-views-home',
   templateUrl: './views-home.component.html',
-  styleUrls: ['./views-home.component.css']
+  styleUrls: ['./views-home.component.css'],
 })
 export class ViewsHomeComponent implements OnInit {
+  stats = [
+    { value: 22, label: '# of Users' },
+    { value: 900, label: 'Revenue' },
+    { value: 50, label: 'Reviews' },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
