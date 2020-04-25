a4e8f41869c1f4ad8d44cceac4462a3e26a3646c
sangTran <sangthien2010@gmail.com>
2020-04-24 21:39:13 -0500

    add openITemIndex to handle active state of each accordion

--- src/app/mods/accordion/accordion.component.ts
+++ src/app/mods/accordion/accordion.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class AccordionComponent implements OnInit {
   @Input() items = [];
+  openItemIndex = 0;
 
   constructor() {}
 
